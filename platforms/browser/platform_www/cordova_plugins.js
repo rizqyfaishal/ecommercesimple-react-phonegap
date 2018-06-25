1cf0e4b48c03c481ef3a7a23de483895d330b2bd
Rizqy Faishal <rizqyfaishal27@gmail.com>
2018-06-25 23:51:20 +0700

    remove xwalk

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -2,8 +2,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "cordova-plugin-crosswalk-webview": "2.4.0"
-}
+{}
 // BOTTOM OF METADATA
 });
